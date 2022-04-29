1a81e84a37643776f3c5fdab131b98ada85f7390
Artem Khasin <akhasin.dev@gmail.com>
2022-04-29 08:56:03 +0300

    init

---
+++ src/components/common/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+    return (
+        <Button variant='contained'>Log In</Button>
+    )
+}
