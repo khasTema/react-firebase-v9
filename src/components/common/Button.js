e17ceeba8258e447373f91d5eca071afdc7bcf67
Artem Khasin <akhasin.dev@gmail.com>
2022-04-29 14:02:16 +0300

    pasword vis done
    password visibility icon done
    some style changes done

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,6 +3,19 @@ import Button from '@mui/material/Button';
 
 export default function BasicButtons({title, handleAction}) {
     return (
-        <Button variant='contained' onClick={handleAction} >{title}</Button>
+        <Button 
+            sx={{
+                color: '#fff',
+                fontWeight: '600',
+                backgroundColor: '#7d41a6',
+                '&:hover': {
+                    backgroundColor: 'orange'
+                }
+            }}
+            variant='contained' 
+            onClick={handleAction} 
+        >
+                {title}
+            </Button>
     )
 }
--- src/components/common/Form.js
+++ src/components/common/Form.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import {TextField, InputAdornment, IconButton} from '@mui/material';
+// import { InputAdornment, IconButton } from "@material-ui/core";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+// import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Button from './Button'
 
 export default function BasicTextFields({title, setEmail, setPassword, handleAction}) {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
 
     return(
-        <div>
+        <Box
+            sx={{
+                width: '90%',
+                maxWidth: '500px',
+                margin: '20px auto',
+                backgroundColor: '#f9ffff',
+                padding: '5em 0',
+                boxShadow: '4px 3px 5px -1px #E0E0E0',
+            }}
+        >
             <div className='heading-container'>
-                <h3>
+                <h3 
+                    style={{
+                        textTransform: 'uppercase',
+                        fontWeight: '800',
+                        color: '#7d41a6'
+                    }}
+                    >
                     {title} Form
                 </h3>
             </div>
             <Box 
                 component='form'
                 sx={{
+                    marginBottom: '2em',
                     '& > :not(style)': { m: 1, width: '25ch' },
                 }}
                 noValidate
@@ -30,13 +51,22 @@ export default function BasicTextFields({title, setEmail, setPassword, handleAct
                 <TextField 
                     id='password' 
                     label='Enter Password' 
-                    type='password'
+                    type={showPassword ? 'text' : 'password'}
                     variant='outlined'
                     onChange={(e) => setPassword(e.target.value)}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position='end'>
+                                <IconButton onClick={handleClickShowPassword}>
+                                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
                 />
             </Box>
 
             <Button title={title} handleAction={handleAction}/>
-        </div>
+        </Box>
     )
 }
